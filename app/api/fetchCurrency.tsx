c455d42cf492961f342e081eb57df5d896926a06
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-12-17 17:29:29 +0000

    complete functionality

--- app/api/fetchCurrency.tsx
+++ app/api/fetchCurrency.tsx
@@ -1,10 +1,13 @@
-export const fetchCurrency = async () => {
+const fetchCurrency = async () => {
   const url =
     "https://gist.githubusercontent.com/manosim/aa81673cf0fe629ea4efb586bb7cfd3c/raw/635f8bcbc87c5907a08afca67ba0ddfe6b4b03f0/currencies.json";
   try {
-    const currencies = await fetch(url);
+    const response = await fetch(url);
+    const currencies = await response.json();
     return currencies;
   } catch (error) {
     console.log(error);
   }
 };
+
+export default fetchCurrency;
--- app/index.tsx
+++ app/index.tsx
@@ -1,49 +1,121 @@
-import { useNavigation } from "expo-router";
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
   TouchableOpacity,
   TextInput,
   FlatList,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
-
-import { fetchCurrency } from "./api/fetchCurrency";
+import { useNavigation } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import fetchCurrency from "./api/fetchCurrency";
 
 export default function Home() {
   const navigation = useNavigation();
 
   const [text, setText] = useState("");
+  const [currencies, setCurrencies] = useState([]);
 
   const onChangeText = (input) => {
     setText(input);
   };
 
-  console.log({ text });
+  useEffect(() => {
+    (async () => {
+      const data = await fetchCurrency();
+      console.log("data => ", data);
+      if (data) setCurrencies(data);
+    })();
+  }, []);
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity
+        style={styles.currencyButton}
+        onPress={() => navigation.navigate("card", { item })}
+      >
+        <View style={styles.currencyWrapper}>
+          <View style={{ flex: 1, flexDirection: "row", gap: 4 }}>
+            <Text>{item.flag || ""}</Text>
+            <Text>{item.code || ""}</Text>
+            <Text>{item.name || ""}</Text>
+          </View>
+          <MaterialIcons name="chevron-right" size={16} color="black" />
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const filteredCurrencies =
+    currencies.filter((el) =>
+      el?.code?.toLowerCase().includes(text.toLowerCase())
+    ) || [];
 
   return (
-    <View style={{ flex: 1, padding: 10 }}>
-      <TextInput
-        style={{
-          borderWidth: 1,
-          borderColor: "black",
-          borderRadius: 10,
-          height: 50,
-          paddingHorizontal: 10,
-        }}
-        onChangeText={onChangeText}
-        value={text}
-      />
-      <Text>All currencies</Text>
-      <FlatList
-        data={DATA}
-        renderItem={({ item }) => <Item title={item.title} />}
-        keyExtractor={(item) => item.id}
-      />
-      <TouchableOpacity onPress={() => navigation.navigate("card")}>
-        <Text>Click me </Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 0}
+      >
+        <View style={styles.innerContainer}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <MaterialIcons
+              name="search"
+              size={24}
+              color="black"
+              style={{ position: "absolute", left: 5 }}
+            />
+            <TextInput
+              style={styles.textInput}
+              onChangeText={onChangeText}
+              value={text}
+            />
+          </View>
+          <Text style={{ fontWeight: 600 }}>All currencies</Text>
+          <FlatList
+            data={filteredCurrencies}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.code}
+          />
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 10,
+    gap: 16,
+  },
+  textInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    height: 50,
+    padding: 10,
+    paddingLeft: 32,
+  },
+  currencyButton: {
+    flex: 1,
+  },
+  currencyWrapper: {
+    flex: 1,
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "gray",
+    justifyContent: "space-between",
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "white",
+  },
+});
